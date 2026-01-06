6a19854d0b74bb57aa31f85dc026419654cce3ce
Kevin Soenandar <kevinsoenandar@Kevins-MacBook-Air-2.local>
2026-01-06 16:24:57 +1100

    Updated selection attributes

--- macros/utils/create_table_copy.sql
+++ macros/utils/create_table_copy.sql
@@ -1,15 +1,16 @@
 {% macro create_table_copy(should_run=var("should_run", False)) %}
 
     {% if execute %}
-        {% do log(selected_resources, info=True) %}
-
         {% if should_run | as_bool and env_var("DBT_CLOUD_INVOCATION_CONTEXT") == "dev" %}
             {% do log(selected_resources, info=True) %}
 
             {% for node in selected_resources %}
-                {% set node_object = graph.nodes.values() | selectattr("resource_type", "equalto", "model") | selectattr("unique_id", "equalto", node) | first %}
+                {% set node_object = graph.nodes.values() 
+                    | selectattr("resource_type", "equalto", "model") 
+                    | selectattr("config.materialized", "equalto", "incremental")
+                    | selectattr("unique_id", "equalto", node) | first %}
                 {% if node_object | length > 0 %}
-                    {# {% do _execute_create_table_query(node_object.database, node_object.schema, node_object.name) %} #}
+                    {% do _execute_create_table_query(node_object.database, node_object.schema, node_object.name) %}
                 {% endif %}
             {% endfor %}
         {% endif %}
@@ -20,7 +21,7 @@
 {% macro _execute_create_table_query(database, schema, table_name) %}
 
     {% set create_table_query %}
-    create table {{ table_name }} like {{ database }}.{{ schema }}.{{ table_name }}
+    create table {{ target.schema }}.{{ table_name }} like {{ database }}.{{ schema }}.{{ table_name }}
     {% endset %}
 
     {% do run_query(create_table_query) %}
