7a8a33e078d45aec471d2751b9d88678499a2a8f
Kevin Soenandar <kevinsoenandar@Kevins-MacBook-Air-2.local>
2026-01-06 16:10:59 +1100

    First commit of the macro

---
+++ macros/utils/create_table_copy.sql
@@ -0,0 +1,27 @@
+{% macro create_table_copy(should_run=var("should_run", False)) %}
+
+    {% if execute %}
+        {% do log(selected_resources, info=True) %}
+
+        {% if should_run | as_bool and env_var("DBT_CLOUD_INVOCATION_CONTEXT") == "dev" %}
+            {% do log(selected_resources, info=True) %}
+
+            {% for node in selected_resources %}
+                {% set node_object = graph.nodes.values() | selectattr("resource_type", "equalto", "model") | selectattr("unique_id", "equalto", node) | first %}
+                {% if node_object | length > 0 %}
+                    {# {% do _execute_create_table_query(node_object.database, node_object.schema, node_object.name) %} #}
+                {% endif %}
+            {% endfor %}
+        {% endif %}
+
+    {% endif %}
+{% endmacro %}
+
+{% macro _execute_create_table_query(database, schema, table_name) %}
+
+    {% set create_table_query %}
+    create table {{ table_name }} like {{ database }}.{{ schema }}.{{ table_name }}
+    {% endset %}
+
+    {% do run_query(create_table_query) %}
+{% endmacro %}
