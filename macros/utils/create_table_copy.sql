2ba733114d668028cc34b4c4acde6b48db7d8a68
Kevin Soenandar <kevinsoenandar@Kevins-MacBook-Air-2.local>
2026-01-06 13:41:31 +1100

    Updated selected_resources reference

--- macros/utils/create_table_copy.sql
+++ macros/utils/create_table_copy.sql
@@ -2,8 +2,8 @@
 
     {% if should_run and env_var("DBT_CLOUD_INVOCATION_CONTEXT") == "ci" %}
 
-        {% for model in changed_models %}
-            {% set node_object = graph.nodes.values() | selectattr("unique_id", "equalto", model) | selectattr("resource_type", "equalto", "model") | first %}
+        {% for node in selected_resources %}
+            {% set node_object = graph.nodes.values() | selectattr("unique_id", "equalto", node) | selectattr("resource_type", "equalto", "model") | first %}
             {% do log(model, info=True) %}
             {% do _execute_create_table_query(node_object.database, node_object.schema, node_object.name) %}
         {% endfor %}
