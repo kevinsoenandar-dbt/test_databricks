1a8ab371f635e9326c7fc2a6e43f191d51bd63e9
Kevin Soenandar <kevinsoenandar@Kevins-MacBook-Air-2.local>
2026-01-06 17:55:16 +1100

    Updated copied relation database and schema

--- macros/utils/create_table_copy.sql
+++ macros/utils/create_table_copy.sql
@@ -12,7 +12,7 @@
                     | selectattr("config.materialized", "equalto", "incremental")
                     | selectattr("unique_id", "equalto", node) | first %}
                 {% if node_object | length > 0 %}
-                    {% do _execute_create_table_query(node_object.database, node_object.schema, node_object.name) %}
+                    {% do _execute_create_table_query(node_object.config.database, node_object.config.schema, node_object.name) %}
                 {% endif %}
             {% endfor %}
         {% endif %}
@@ -22,7 +22,7 @@
 
 {% macro _create_temp_pr_schema() %}
     {% set create_temp_pr_schema_query %}
-    create schema if not exists {{ target.schema }}
+    create schema if not exists {{ target.database }}.{{ target.schema }}
     {% endset %}
 
     {% do run_query(create_temp_pr_schema_query) %}
