ec9cd8b7628572c7c46c9a18783b73cd05a6a2bc
Kevin Soenandar <kevinsoenandar@Kevins-MacBook-Air-2.local>
2026-01-07 11:06:41 +1100

    Added documentations

--- macros/utils/create_table_copy.sql
+++ macros/utils/create_table_copy.sql
@@ -1,3 +1,28 @@
+{#
+
+    This macro is used to create a table copy of the selected resources in a dbt job command. This macro is meant to be used to circumvent the shallow / deep cloning 
+    limitation on tables with RLS and column masking applied in Databricks.
+
+    The macro works by:
+    1. Accessing the selected_resources context to get the list of nodes to create table copies for.
+    2. For each selected node, it will filter out non-model nodes and non-incremental models.
+    3. For each incremental model node, it will check the existence of a defer_relation object
+    4. If a defer_relation object is found, it will create a table copy of the node using the defer_relation object.
+    5. If no defer_relation object is found, it will skip the node.
+
+    To use the macro, add the following to the dbt_project.yml file:
+    on-run-start:
+      - "{{ create_table_copy(var('should_run', False)) }}"
+
+    This will ensure that by default, the macro will NOT run. Then, in the CI job setting of the dbt platform,
+    update the job command from `dbt clone --select state:modified+,config.materialized:incremental,state:old` to `dbt compile --select state:modified+,state:old --vars '{"should_run": True}'`
+    
+    Notes:
+    1. The macro will handle the temporary schema creation as the schema creation is done _after_ the on-run-start hook is executed by default
+    2. Do not add the `config.materialized:incremental` flag to the node selection in the job command as this will skip the macro entirely from running
+
+#}
+
 {% macro create_table_copy(should_run=var("should_run", False)) %}
 
     {% if execute %}
@@ -39,5 +64,6 @@
     create table {{ target.schema }}.{{ table_name }} like {{ defer_relation }}
     {% endset %}
 
+    {% do log("Executing create table query...", info=True) %}
     {% do run_query(create_table_query) %}
 {% endmacro %}
